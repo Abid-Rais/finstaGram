aa151fe8243885fa26553ab23ffc941787289589
Abid-Rais <abidrais1843@gmail.com>
2019-11-22 14:29:52 -0500

    Added views for login and register

--- __init__.py
+++ __init__.py
@@ -18,3 +18,42 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    username = request.form['username']
+    password = request.form['password']
+
+    cursor = conn.cursor()
+    query = 'SELECT * FROM user WHERE username = %s and password = %s'
+    cursor.execute(query, (username, password))
+    data = cursor.fetchone()
+    cursor.close()
+    error = None
+    if(data):
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
+
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+    username = request.form['username']
+    password = request.form['password']
+
+    cursor = conn.cursor()
+    query = 'SELECT * FROM user WHERE username = %s'
+    cursor.execute(query, (username))
+    data = cursor.fetchone()
+    error = None
+    if (data):
+        error = "This user already exists"
+        return render_template('register.html', error = error)
+    else:
+        ins = 'INSERT INTO user VALUES(%s, %s)'
+        cursor.execute(ins, (username, password))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
