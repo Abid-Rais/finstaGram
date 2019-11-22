f90c44690bdea302a2df0c6c3f8fbb470dfab8ac
Abid-Rais <abidrais1843@gmail.com>
2019-11-22 14:49:51 -0500

    Added more views to __init__.py

--- __init__.py
+++ __init__.py
@@ -11,13 +11,16 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
 
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
@@ -36,6 +39,7 @@ def loginAuth():
     else:
         error = 'Invalid login or username'
         return render_template('login.html', error=error)
+
 
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
@@ -57,3 +61,56 @@ def registerAuth():
         cursor.close()
         return render_template('index.html')
 
+
+@app.route('/home')
+def home():
+    user = session['username']
+    cursor = conn.cursor();
+    query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
+    cursor.execute(query, (user))
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('home.html', username=user, posts=data)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    username = session['username']
+    cursor = conn.cursor();
+    blog = request.form['blog']
+    query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
+    cursor.execute(query, (blog, username))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
+
+@app.route('/select_blogger')
+def select_blogger():
+    # check that user is logged in
+    # username = session['username']
+    # should throw exception if username not found
+
+    cursor = conn.cursor();
+    query = 'SELECT DISTINCT username FROM blog'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('select_blogger.html', user_list=data)
+
+
+@app.route('/show_posts', methods=["GET", "POST"])
+def show_posts():
+    poster = request.args['poster']
+    cursor = conn.cursor();
+    query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
+    cursor.execute(query, poster)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('show_posts.html', poster_name=poster, posts=data)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
